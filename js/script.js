86710d5ccdd3c0e5be215a57d60a6b552d7f768e
Dadee Samuel <dadeesamuel@gmail.com>
2023-03-17 08:58:20 +0100

    create a account display

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Business Logic for Ticket ---------
 function User() {
     this.accounts = {};
-    this.currentId = 0;
+    this.currentId = 2013000001;
 }
 
 User.prototype.addAccount = function (account) {
@@ -29,16 +29,18 @@ User.prototype.deleteAccount = function (id) {
     return true;
 };
 
+
+
 // Business Logic for account ---------
-function Account(accountName1, accountName2, address, age, bankName, balance) {
+function Account(accountName1, accountName2, address, age, bankName, iDeposit , deposit, withdraw) {
     this.accountName1 = accountName1;
     this.accountName2 = accountName2;
     this.address = address;
     this.age = age;
     this.bankName = bankName;
-    this.balance = balance;
-
-    this.acctNumber = 2012345678;
+    this.iDeposit = iDeposit ;
+    this.deposit = deposit;
+    this.withdraw = withdraw;
 
 }
 
@@ -46,45 +48,114 @@ Account.prototype.fullName = function () {
     return this.accountName1 + " " + this.accountName2;
     ;
 };
-Account.prototype.accountNumber = function () {
 
-    return this.acctNumber++;
-
-};
-
-
-    // function(1) for take input value
 function getInputNumber(id) {
-        const amount = document.getElementById(id).value;
-        const amountNumber = parseFloat(amount);
-        return amountNumber;
-    }
-    // function(2) for take inner text value
-    function getInnerText(id) {
-        let currentBalance = document.getElementById(id).innerText;
-        let balanceNumber = parseFloat(currentBalance);
-        return balanceNumber;
+    const amount = document.getElementById(id).value;
+
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
 }
-    
-    document.getElementById('addDepositBtn').addEventListener('click', function () {
-        const deposit = getInputNumber('depositAmount');
+function getInnerText(id) {
+    let currentBalance = document.getElementById(id).innerText;
+    let balanceNumber = parseFloat(0 + currentBalance );
+    return balanceNumber;
+}
+
+
+
+
+
+// user interface logic
+let user = new User();
+function displayAccountDetails(userToDisplay) {
+    let accountList = $("ol#accounts");
+    let htmlForAccountInfo = "";
+    Object.keys(userToDisplay.accounts).forEach(function (key) {
+        const account = userToDisplay.findAccount(key);
+        htmlForAccountInfo += "<li id=" + account.id + ">" + account.fullName() + " " + account.id + " " + account.iDeposit + "</li>";
+    });
+    accountList.html(htmlForAccountInfo);
+}
+
+
+function showAccount(accountId) {
+    const account = user.findAccount(accountId);
+    $("#show-account").toggle();
+    $("#user-name").text(account.fullName());
+    $("#user-account").html(account.id);
+    $(".user-bank").html(account.bankName);
+    $("#currentDeposit").html(account.iDeposit);
+    $("#currentWithdraw").html(account.withdraw);
+    $("#currentBalance").html(account.iDeposit);
+
+
+    let buttons = $("#buttons");
+    buttons.empty();
+    buttons.append("<button class='deleteButton' id=" + " " + account.id + ">Delete Account</button>");
+}
+
+
+
+
+
+function attachContactListeners() {
+    $("ol#accounts").on("click", "li", function () {
+        showAccount(this.id);
+    });
+    $("#addDepositBtn").on("click", function () {
+        const deposit = getInputNumber('depositAmount')
         const depositAmount = getInnerText('currentDeposit');
         let depositAdBalance = deposit + depositAmount;
-
-        document.getElementById('currentDeposit').innerText = depositAdBalance;
-        document.getElementById('depositAmount').value = "";
-
+        $("#currentDeposit").html(depositAdBalance);
+        $("#depositAmount").val("");
         const balance = getInnerText('currentBalance');
         let totalBalance = balance + deposit;
-        document.getElementById('currentBalance').innerText = totalBalance;
-    })
-    document.getElementById('withdrawBtn').addEventListener('click', function () {
-        const withdraw = getInputNumber('withdrawAdd');
-        const withdrawNumber = getInnerText('withdrawMony');
-        const balanceNumber = getInnerText('currentBalance');
-        const totalWithdraw = withdrawNumber + withdraw;
-
-        document.getElementById('withdrawMony').innerText = totalWithdraw;
-        document.getElementById('currentBalance').innerText = balanceNumber - withdraw;
-        document.getElementById('withdrawAdd').value = "";
+        $("#currentBalance").html(totalBalance);
+        console.log(deposit)
+        console.log(depositAmount)
+        console.log(depositAdBalance)
     });
+    $("#withdrawBtn").on("click", function () {
+        const withdraw = getInputNumber('withdrawAmount');
+        const withdrawNumber = getInnerText('currentWithdraw');
+        let totalWithdraw = withdrawNumber + withdraw;
+        $('#currentWithdraw').html(totalWithdraw);
+        $('#withdrawAmount').val("");
+        const balanceNumber = getInnerText('currentBalance');
+        let totalBalance = balanceNumber - withdraw;
+        $('#currentBalance').html(totalBalance);
+        console.log(withdraw)
+        console.log(withdrawNumber)
+        console.log(totalWithdraw)
+
+    });
+    $("#buttons").on("click", ".deleteButton", function () {
+        user.deleteAccount(this.id);
+        $("#show-account").hide();
+        displayAccountDetails(user);
+    });
+  
+}
+
+$(document).ready(function () {
+    attachContactListeners();
+
+    // customer details form
+    $(".customer-form").submit(function (event) {
+        event.preventDefault();
+        const inputtedSurname = $("input.surname").val();
+        const inputtedFirstName = $("input.firstname").val();
+        const inputtedAddress = $("input.address").val();
+        const inputtedAge = $("input.age").val();
+        const inputtedBank = $("select.bank").val().toUpperCase();
+        const initialDeposit = parseInt($("input.i-deposit").val());
+        const deposit = parseInt($("input#depositAmount").val());
+        const withdraw = parseInt($("input#withdrawAmount").val());
+        let newAccount = new Account(inputtedSurname, inputtedFirstName, inputtedAddress, inputtedAge, inputtedBank, initialDeposit, deposit, withdraw);
+        user.addAccount(newAccount);
+        displayAccountDetails(user);
+        console.log(user.accounts);
+    });
+});
+
+
